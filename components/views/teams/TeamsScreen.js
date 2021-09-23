08e1a3fa6dd527edff868355a746182ce535c737
akhfas <zakaria@aksaramaya.com>
2021-09-23 22:36:23 +0700

    get data team

--- components/views/teams/TeamsScreen.js
+++ components/views/teams/TeamsScreen.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements/dist/card/Card';
-import { allteams } from '../../../data';
+import { useQuery, gql } from '@apollo/client';
 
-const TeamsScreen = () => (
-  <View style={styles.container}>
-    <ScrollView>
-      <Text style={styles.title}>All Teams</Text>
-      {allteams.map((item) => (
-        <View key={item.id}>
-          <Text style={styles.region}>{item.region}</Text>
-          <Card containerStyle={styles.card}>
-            <View style={styles.item}>
-              {item.logo.map((logo) => (
-                <View>
-                  <Image source={logo} />
-                </View>
-              ))}
-            </View>
-          </Card>
-        </View>
-      ))}
-    </ScrollView>
-  </View>
-);
+const TeamsScreen = () => {
+  const QUERY = gql`
+    query {
+      region {
+        id
+        name
+        teams {
+          id
+          image_url
+        }
+      }
+    }
+  `;
+
+  const { data } = useQuery(QUERY);
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <Text style={styles.title}>All Teams</Text>
+        {data?.region?.map((item) => (
+          <View key={item.id}>
+            <Text style={styles.region}>{item.name}</Text>
+            <Card containerStyle={styles.card}>
+              <View style={styles.item}>
+                {item?.teams?.map((e) => (
+                  <View>
+                    <Image
+                      source={{
+                        uri: e.image_url,
+                      }}
+                      style={styles.image}
+                    />
+                  </View>
+                ))}
+              </View>
+            </Card>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
 
 export default TeamsScreen;
 
@@ -36,23 +58,29 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: '700',
     color: 'white',
-    marginTop: 40,
-    marginLeft: 20,
+    marginTop: '5%',
+    marginLeft: '5%',
   },
   region: {
     color: 'white',
     marginLeft: 20,
     fontWeight: '500',
-    marginTop: 20,
+    marginTop: '5%',
     fontSize: 16,
   },
   card: {
     elevation: 0,
     backgroundColor: 'white',
-    borderRadius: 10,
+    borderRadius: 8,
+    marginBottom: 10,
   },
   item: {
     flexDirection: 'row',
     justifyContent: 'space-around',
+  },
+  image: {
+    width: 30,
+    height: 30,
+    borderRadius: 50,
   },
 });
